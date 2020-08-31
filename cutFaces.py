f379b013c19451c15fe874862be1158217342384
Guilherme Mello <guilhermecfmello@gmail.com>
2020-08-31 20:52:15 -0300

    Cropping faces ok

--- cutFaces.py
+++ cutFaces.py
@@ -1,6 +1,7 @@
 # xml import
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+from xml.etree.ElementTree import tostring
 # json import
 import json
 # directory navigate import
@@ -23,40 +24,57 @@ def getCoordinates(object):
   xMax = int(bndbox.find('xmax').text)
   yMin = int(bndbox.find('ymin').text)
   yMax = int(bndbox.find('ymax').text)
-  # print(object.text)
-  # exit()
-
   return xMin, yMin, xMax, yMax
 
+path = 'guilhermecfmello/training/'
+destinyPath = 'guilhermecfmello/cropped/training/'
 
-validationAnn = 'guilhermecfmello/validation/annotations/'
-validationImg = 'guilhermecfmello/validation/images/'
-croppedAnn = 'guilhermecfmello/cropped/validation/annotations/'
-validationImg = 'guilhermecfmello/cropped/validation/images/'
+sourceAnn = path + 'annotations/'
+sourceImg = path + 'images/'
 
+destinyAnn = destinyPath + 'annotations/'
+destinyImg = destinyPath + 'images/'
 
-annsList = [f for f in listdir(validationAnn) if isfile(join(validationAnn, f))]
+def newXml(fileName, width, height, obj):
+  newAnn = ET.Element('annotation')
+  ET.SubElement(newAnn, 'folder').text = 'images'
+  ET.SubElement(newAnn, 'filename').text = fileName
+  size = ET.SubElement(newAnn, 'size')
+  ET.SubElement(size, 'width').text = str(width)
+  ET.SubElement(size, 'height').text = str(height)
+  ET.SubElement(size, 'depth').text = '3'
+  ET.SubElement(newAnn, 'segmented').text = '0'
+  newObj = ET.SubElement(newAnn, 'object')
+  newObj.extend(obj)
+  return newAnn
+
+annsList = [f for f in listdir(sourceAnn) if isfile(join(sourceAnn, f))]
 
 for ann in annsList:
-  if ann == '5619w.xml':
-    with open(validationAnn+ann,'r') as file:
-      tree = ET.fromstring(file.read())
-      imgName = tree.find('filename').text
-      img = Image.open(validationImg+imgName)
+  with open(sourceAnn+ann,'r') as file:
+    tree = ET.fromstring(file.read())
+    imgName = tree.find('filename').text
+    img = Image.open(sourceImg+imgName)
+    dot = imgName.find('.')
+    i = 1
+    for obj in tree.iter('object'):
+      baseName = imgName[:dot] + 'face-' + str(i)
+      print('Creating ' + baseName + '...')
+      xMin, Ymin, xMax, yMax = getCoordinates(obj)
+      try:
+        tinyImg = img.crop((xMin, Ymin, xMax, yMax))
+        tinyImg.save(destinyImg + baseName + imgName[dot:])
+        width, height = tinyImg.size
+        newAnn = newXml(baseName, width, height, obj)
+        xmlStr = minidom.parseString(ET.tostring(newAnn)).toprettyxml(indent='  ')
+        with open(destinyAnn + baseName + '.xml', 'w') as f:
+          # printing xml as string without header 'version'
+          f.write(xmlStr[23:])
+        i = i + 1
+      except:
+        print("@@@@@@@@@@@@@@@@@@@ ERROR ON CROP @@@@@@@@@@@@@@@@@@@")
+        with open('errors-crop.log', 'w+') as f:
+          f.write('error on: ' + baseName)
       
-      # print(tree.findall('object')[1].text)
-      # exit()
-      for obj in tree.iter('object'):
-        # print(getCoordinates(obj))
-        # print(obj.find('bndbox').find('xmin').text)
-        # exit(0)
-        xMin, Ymin, xMax, yMax = getCoordinates(obj)
-        img2 = img.crop((xMin, Ymin, xMax, yMax))
-        img2.show()
-        # exit()
-      
-      exit(0)
-      # objs = tree.findall('object')
-      # print(len(objs))
 
 
